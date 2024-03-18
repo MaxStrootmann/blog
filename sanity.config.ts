75b4ea984fd374c5cd5dfaf63b074bbfb4b78a72
Max Strootmann <max@nngrafischontwerp.nl>
2024-03-18 06:07:37 +0100

    update, remove preview from studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,13 +4,7 @@
 
 import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
-import { deskTool } from 'sanity/desk'
-import {
-  defineUrlResolver,
-  Iframe,
-  IframeOptions,
-} from 'sanity-plugin-iframe-pane'
-import { previewUrl } from 'sanity-plugin-iframe-pane/preview-url'
+import { structureTool } from 'sanity/structure'
 
 // see https://www.sanity.io/docs/api-versioning for how versioning works
 import {
@@ -21,15 +15,6 @@ import {
 } from '~/lib/sanity.api'
 import { schema } from '~/schemas'
 
-const iframeOptions = {
-  url: defineUrlResolver({
-    base: '/api/draft',
-    requiresSlug: ['post'],
-  }),
-  urlSecretId: previewSecretId,
-  reload: { button: true },
-} satisfies IframeOptions
-
 export default defineConfig({
   basePath: '/studio',
   name: 'project-name',
@@ -39,7 +24,7 @@ export default defineConfig({
   //edit schemas in './src/schemas'
   schema,
   plugins: [
-    deskTool({
+    structureTool({
       // `defaultDocumentNode` is responsible for adding a “Preview” tab to the document pane
       // You can add any React component to `S.view.component` and it will be rendered in the pane
       // and have access to content in the form in real-time.
@@ -49,16 +34,8 @@ export default defineConfig({
         return S.document().views([
           // Default form view
           S.view.form(),
-          // Preview
-          S.view.component(Iframe).options(iframeOptions).title('Preview'),
         ])
       },
-    }),
-    // Add the "Open preview" action
-    previewUrl({
-      base: '/api/draft',
-      requiresSlug: ['post'],
-      urlSecretId: previewSecretId,
     }),
     // Vision lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
